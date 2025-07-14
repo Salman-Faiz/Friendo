fd1e2c3088585e95437cdf2d3fa89378723167d8
Salman-Faiz <salmanfaiz.cse@gmail.com>
2025-07-14 17:16:13 +0600

    loginForm created with react hook form

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,36 +1,55 @@
-
+import Field from '../common/Field'
+import { useForm } from 'react-hook-form'
 
 const LoginForm = () => {
-     return (
-          <form className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]">
-          <div className="form-control">
-            <label className="auth-label" htmlFor="email">Email</label>
-            <input
-              className="auth-input"
-              name="email"
-              type="email"
-              id="email"
-            />
-          </div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
 
-          <div className="form-control">
-            <label className="auth-label" htmlFor="password">Password</label>
-            <input
-              className="auth-input"
-              name="password"
-              type="password"
-              id="password"
-            />
-          </div>
+  const submitForm = formData => {
+    console.log(formData)
+  }
 
-          <button
-            className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
-            type="submit"
-          >
-            Login
-          </button>
-        </form>
-     );
-};
+  return (
+    <form
+      className='border-b border-[#3F3F3F] pb-10 lg:pb-[60px]'
+      onSubmit={handleSubmit(submitForm)}
+    >
+      <Field label='Email' error={errors?.email}>
+        <input 
+          {...register("email", { required: 'Email is required' })}
+          className={`auth-input ${errors?.email ? "border-red-500" : "border-gray-200"}`}
+          type='email'
+          id='email'
+        />
+      </Field>
 
-export default LoginForm;
+      <Field label='Password' error={errors?.password}>
+        <input
+          {...register("password", { required: 'Password is required',
+            minLength:{
+              value:8,
+              message:"Your password must be at least 8 characters"
+            }
+           })}
+          className={`auth-input ${errors?.password ? "border-red-500" : "border-gray-200"}`}
+          type='password'
+          id='password'
+        />
+      </Field>
+
+   <Field>
+       <button
+        className='auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90'
+        type='submit'
+      >
+        Login
+      </button>
+   </Field>
+    </form>
+  )
+}
+
+export default LoginForm
--- src/components/common/Field.jsx
+++ src/components/common/Field.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
-const Field = () => {
-     return (
-          <div>
-               
-          </div>
-     );
+const Field = ({ label, children, htmlFor, error }) => {
+  const getChildId = (children) => {
+    const child = React.Children.only(children);
+    if ("id" in child?.props) {
+      return child.props.id;
+    }
+    return undefined;
+  };
+
+  const id = htmlFor || getChildId(children);
+
+  return (
+    <div className='form-control'>
+      {label && <label htmlFor={id} className='auth-label'>{label}</label>}
+      {children}
+      {!!error && <div role='alert' className='text-red-600'>{error.message}</div>}
+    </div>
+  );
 };
 
 export default Field;
