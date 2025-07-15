966886a0454f7209074ad97eb25984765a92e9c5
Salman-Faiz <salmanfaiz.cse@gmail.com>
2025-07-15 15:07:14 +0600

    header

---
+++ src/components/auth/Logout.jsx
@@ -0,0 +1,15 @@
+import logout from '../../assets/icons/logout.svg'
+import { useNavigate } from 'react-router-dom';
+const Logout = () => {
+     const navigate = useNavigate();
+     const handleLogout = ()=>{
+          navigate('/login')
+     }
+     return (
+          <button className="icon-btn" onClick={handleLogout}>
+          <img src={logout} alt="Logout" />
+        </button>
+     );
+};
+
+export default Logout;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
-
+import Header from "../components/common/Header";
 
 const HomePage = () => {
      return (
           <div className="text-red-300 text-3xl">
+               <Header/>
               home
           </div>
      );
